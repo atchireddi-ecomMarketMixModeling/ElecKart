2d0694e0867b257b37f70672672665e36abf4557
atchireddi@gmail.com <atchireddi@gmail.com>
2017-05-06 08:43:46 -0700

    updated with R Markdown

--- model.R
+++ model.R
@@ -1,81 +1,170 @@
+#' ---
+#' title: 'MarketMixModeling Models'
+#' author: 'Atchireddy chavva'
+#' output: pdf_document
+#' ---
 
-```{r, warning=FALSE, message=FALSE, title:'MarketMixModeling_Model',}
 
-# ***************************************************************************
-#                   Load Library ----
-# ***************************************************************************
+
+ 
+ 
+#+ Load_Library ---- 
+#' ## Load Libraries:
+#'
+#'  Load required libraries. Will use `stepAIC`  from `MASS` package for model pruning.
+#' 
+
+#+ warning=FALSE, message=FALSE
 library(MASS)
+library(stargazer)
 
 
-# ***************************************************************************
-#                   LOad Data ----
-# ***************************************************************************
+
+
+
+
+#+ Load Data ---- 
+#' ## Load Data:
+#'
+#'  Load blended data from sales and marketing datasets. Data is throughly
+#' cleaned, pre-processed for model building. Refer to `DataCleaning.R`
+#' script for data preparation steps. For this capstone project we limit
+#' our model building focus to `camera_accessory`, `Home_audio` and 
+#' `Gaming_accessory` product sub-categories. For simplicity will start
+#' Linear model building with numerical features, later will consider 
+#' categorical features.
+#' \vspace{4pt}   
+
+#+ load_data,
 camera_accessory_data_nrm <- read.csv('./intrim/cameraAccessory.csv')
-home_audio_data_nrm <-       read.csv('./intrim/homeAudio.csv')
+home_audio_data_nrm       <- read.csv('./intrim/homeAudio.csv')
 gaming_accessory_data_nrm <- read.csv('./intrim/gamingAccessory')
 
 
-# ***************************************************************************
-#                        LINEAR MODEL : Camera_accessory ----
-# ***************************************************************************
+#' \vspace{4pt}
+#' Lets preview the dataset structure. `gmv` gross merchandise values is our target
+#' variable, which we would like to maximize with optimal marketing spend across 
+#' different marketing levers, `discount` is one of the KPI derived from sales data, 
+#' bunch of other features from marketing spend and NPS datasets.
+#' \vspace{8pt}
 
-# do we really need to predict something, 
-# I feel, we are trying to explain effects of indenpendant variables
-# set.seed(100)    
-indices=sample(1:nrow(camera_accessory_data_nrm),1.0*nrow(camera_accessory_data_nrm))
-train=camera_accessory_data_nrm[indices,]
-test=camera_accessory_data_nrm[-indices,]
 
-# Initial Model
-model_cam1 <- lm(gmv~ .,data=train)
+#'
+#' \footnotesize
+str(camera_accessory_data_nrm)
+#' \vspace{12pt}
+#' 
+#' **Features distribution:** Look at features statistical distributions
+stargazer(camera_accessory_data_nrm,type='text')
+#' \normalsize
 
-# summary(model_cam1)
+
+#' \vspace{12pt}
+#+ Training and Validation Data ----
+#' \vspace{8pt}
+#' ## Train and Validation Data:
+#' 
+#' With one year Sales and Marketing Data, we have 52 obeservations aggreagted
+#' at weekly level for each sub-category. Splitting data into training and validation
+#' sets would further reduce the training sample size, Moreover the task at hand
+#' is to figure out most influential marketing leavers for optimial marketing spend.
+#' The goal is to explain the influence of features rather predicting any quantities
+#' we can safely utilize the whole dataset for training.
+
+
+
+
+
+
+
+#' \newpage
+#+ Model Building ----
+#' ## **Model Building - Linear Model:**
+#' 
+#'
+#' \vspace{8pt}  
+#'  
+#'  ###### Assumptions:
+#'  For simplicity, will consider, each sub-category sales affected from overall marketing spend,
+#'  where in reality, only a portion of the marketing spend would have been alloted for promoting
+#'  a certain product category.
+
+
+
+
+#+ . . . . Camera Accessory ----
+#' #### Camera Accesory:
+#' 
+#+ . . . . . . . . Initial Linear Model ----
+#' ###### Initial Linear Model
+model_cam1 <- lm(gmv~ .,data=camera_accessory_data_nrm)
+
+#+ . . . . . . . . Auto-Otimize Model ----
+#' ###### Auto-Optimize Model
 step_cam <- stepAIC(model_cam1, direction = "both",trace=FALSE,k=2)
+ 
+#' **Summary**
+#' \footnotesize
+stargazer(model_cam1,step_cam, align = TRUE, type = 'text',
+          title='Linear Regression Results', single.row=TRUE)
+#' \normalsize
+
+#' \vspace{8pt}
+#' ##### **Understanding Model:**
+#' \vspace{8pt}
+#' 
+#' Linear model could explain 56% of revenue from marketing expenditure, but some 
+#' of the significant features like `TV spending` has negative coefficient term. 
+#' If we were to explain this, it should mean, `reducing the TV marketing spend would 
+#' increase camera Accessory sales`, which doesn't make sense. we can further optimize
+#' model by exploring multi-collinearity and pruning features. Hopefully we may end
+#' up with a model with better R-Square value and co-efficient terms which makes sense.
 
 
-summary(step_cam)
 
+#' \newpage
+#+ . . . . Gaming Accessory ----
+#' #### Gaming Accesory:
+#' 
+#+ . . . . . . . . Initial Linear Model ----
+#' ###### Initial Model
+model_ga1 <- lm(gmv~ .,data=gaming_accessory_data_nrm)
 
-
-
-
-# ***************************************************************************
-#                        LINEAR MODEL : gaming_accessory ----
-# ***************************************************************************
-
-indices=sample(1:nrow(home_audio_data_nrm),1.0*nrow(home_audio_data_nrm))
-train=home_audio_data_nrm[indices,]
-test=home_audio_data_nrm[-indices,]
-
-# Initial Model
-model_ga1 <- lm(gmv~ .,data=train)
-
-# summary(model_ga1)
+#+ . . . . . . . . Auto-Optimize Model ----
+#' ###### Auto-Optimize Model
 step_ga <- stepAIC(model_ga1, direction = "both",trace=FALSE)
 
-summary(step_ga)
+ 
+#' \footnotesize
+stargazer(model_ga1,step_ga, align = TRUE, type = 'text',
+          title='Linear Regression Results', single.row=TRUE)
+#' \normalsize
 
 
 
 
+#' \newpage
+#+ . . . . Home Accessory ----
+#' #### Home Accessory
+#' 
+#+ . . . . . . . . Initial Linear Model ----
+#' ###### Initial Model
+model_ha1 <- lm(gmv~ .,data=home_audio_data_nrm)
 
-
-# ***************************************************************************
-#                        LINEAR MODEL : home_audio ----
-# ***************************************************************************
-
-indices=sample(1:nrow(gaming_accessory_data_nrm),1.0*nrow(gaming_accessory_data_nrm))
-train=gaming_accessory_data_nrm[indices,]
-test=gaming_accessory_data_nrm[-indices,]
-
-# Initial Model
-model_ha1 <- lm(gmv~ .,data=train)
-
-# summary(model_ha1)
+#+ . . . . . . . . Auto-Optimize Model ----
+#' ###### Auto-Opitimize Model
 step_ha <- stepAIC(model_ha1, direction = "both",trace=FALSE)
 
-summary(step_ha)
+#' \normalsize
+#' \footnotesize
+#' **Summary**
+stargazer(model_ha1,step_ha, align = TRUE, type = 'text',
+          title='Linear Regression Results', single.row=TRUE)
+#' \normalsize
 
 
 
-```
+
+
+#+ Observations ----
+#' ## Observations:
